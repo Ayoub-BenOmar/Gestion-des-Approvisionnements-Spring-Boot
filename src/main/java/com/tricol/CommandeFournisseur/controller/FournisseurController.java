10ef4ca5a74e603f04f55f79aa127ad3228671d3
ayuub <ayoubbenomar1230@gmail.com>
2025-10-31 12:21:03 +0100

    added the update founisseur fucntion

--- src/main/java/com/tricol/CommandeFournisseur/controller/FournisseurController.java
+++ src/main/java/com/tricol/CommandeFournisseur/controller/FournisseurController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/fournisseurs")
@@ -26,5 +27,35 @@ public class FournisseurController {
     public ResponseEntity<Fournisseur> create(@RequestBody Fournisseur fournisseur) {
         Fournisseur saved = service.save(fournisseur);
         return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Integer id){
+        Optional<Fournisseur> fournisseur = service.findById(id);
+        if (fournisseur.isPresent()){
+            return ResponseEntity.ok(fournisseur.get());
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Fournisseur wih the id: " + id + "not found.");
+        }
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable Integer id, @RequestBody Fournisseur fournisseur){
+        Optional<Fournisseur> found = service.findById(id);
+        if (found.isPresent()){
+            Fournisseur exist = found.get();
+            exist.setSociete(fournisseur.getSociete());
+            exist.setAdresse(fournisseur.getAdresse());
+            exist.setContact(fournisseur.getContact());
+            exist.setEmail(fournisseur.getEmail());
+            exist.setTelephone(fournisseur.getTelephone());
+            exist.setVille(fournisseur.getVille());
+            exist.setICE(fournisseur.getICE());
+
+            service.update(exist);
+            return "Fournisseur updated.";
+        }else {
+            return "Fournisseur not found";
+        }
     }
 }
--- src/main/java/com/tricol/CommandeFournisseur/service/FournisseurService.java
+++ src/main/java/com/tricol/CommandeFournisseur/service/FournisseurService.java
@@ -2,8 +2,13 @@ package com.tricol.CommandeFournisseur.service;
 
 import com.tricol.CommandeFournisseur.entities.Fournisseur;
 import com.tricol.CommandeFournisseur.repository.FournisseurRepository;
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FournisseurService {
@@ -19,5 +24,17 @@ public class FournisseurService {
 
     public Fournisseur save(Fournisseur fournisseur) {
         return repository.save(fournisseur);
+    }
+
+    public Optional<Fournisseur> findById(Integer id){
+        if (id != null){
+            return repository.findById(id);
+        }else {
+            return Optional.empty();
+        }
+    }
+
+    public void update(Fournisseur fournisseur){
+        repository.save(fournisseur);
     }
 }
